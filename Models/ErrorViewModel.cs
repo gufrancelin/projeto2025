1d44fa95e1c2df4305559ad563bcc7747a1d9828
gufrancelina <gustavo.francelin@sp.senai.br>
2025-03-20 10:56:59 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projeto2025novo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
